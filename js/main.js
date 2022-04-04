54cb539fd278c4e96f89fac36d7631873ed3aa9f
Mike <maquinde@users.noreply.github.com>
2022-04-03 19:36:50 -0700

    added psuedo code for button submission

--- js/main.js
+++ js/main.js
@@ -1,3 +1,16 @@
+function submission(this){
+    let content = this.textContent;
+    //eventListener on button
+    //check if user selected an option
+        //if no option, show error message
+        //if option, proceed
+    //add textContent to span
+    //add display class to card
+    //remove display class from thank-you
+}
+
+
+
 //This is for the rating selection
 
 //assigns all rating classes to a constant variable
@@ -6,14 +19,15 @@ const choices = document.querySelectorAll('.one, .two, .three, .four, .five');
 //loops through all the items in variable
 for (let choice of choices){
     choice.addEventListener('click', function handleClick() {
+        //on click, loops through all choices and removes .active class
         for(let choice of choices){
             choice.classList.remove('active');
             console.log('active class removed');
         }
 
+        //on click, adds active to selected choice
         this.classList.add('active');
+        //try getting the 'this' element contents and store in variable
         console.log('active class added');
     });
 }
-
-
