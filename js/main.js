3c7b68c68d8b9913d7a3f22f07377aba3708dd41
Mike <maquinde@users.noreply.github.com>
2022-04-05 06:51:09 -0700

    Having issues injecting the users selection into a span

--- js/main.js
+++ js/main.js
@@ -1,6 +1,12 @@
 //This is for the rating selection
 //assigns all rating classes to a constant variable
 const choices = document.querySelectorAll('.one, .two, .three, .four, .five');
+const submit = document.querySelector('.submit-button');
+const mainCard = document.querySelector('.main-card');
+const thankYou = document.querySelector('.thank-you');
+const span = document.getElementById('#rating-selection');
+let rating;
+
 
 //loops through all the items in variable
 for (let choice of choices){
@@ -14,44 +20,41 @@ for (let choice of choices){
         //on click, adds active to selected choice
         this.classList.add('active');
         console.log('active class added');
+        rating = Number(this.textContent);
+        console.log(rating);
+        return rating;
     });
 }
 
+//For the submit button
+submit.addEventListener('click', function(){
+    if(rating){
+        span.textContent = 'TEST'; //fails here
+    }
+
+    else {
+        alert('Please make a selection');
+
+    }
+});
 
 
 
 //Not my code
-'use strict';
+// 'use strict';
 
-const ratingCard = document.querySelector('.rating-card');
-const thankYouCard = document.querySelector('.thank-you-card');
-const submitButton = document.querySelector('.submit');
-const paragraphText = document.querySelector('.printed-result');
-const ratingCircles = document.querySelectorAll('.circle');
-let rating;
+// const ratingCard = document.querySelector('.rating-card');
+// const thankYouCard = document.querySelector('.thank-you-card');
+// const submitButton = document.querySelector('.submit');
+// const paragraphText = document.querySelector('.printed-result');
+// let rating = "";
 
-ratingCircles.forEach(function (circle) {
-  circle.addEventListener('click', function () {
-    ratingCircles.forEach(function (ratingCircle) {
-      if (circle === ratingCircle) {
-        ratingCircle.classList.add('submit-clicked');
-      } else {
-        ratingCircle.classList.remove('submit-clicked');
-      }
-    });
-    rating = Number(circle.textContent);
-  });
-});
-
-
-
-
-submitButton.addEventListener('click', function () {
-  if (!rating) {
-    alert('Please select a rating before proceeding');
-  } else {
-    ratingCard.classList.add('hidden');
-    thankYouCard.classList.remove('hidden');
-    paragraphText.textContent = `You selected ${rating} out of 5`;
-  }
-});
+// submitButton.addEventListener('click', function () {
+//   if (!rating) {
+//     alert('Please select a rating before proceeding');
+//   } else {
+//     ratingCard.classList.add('hidden');
+//     thankYouCard.classList.remove('hidden');
+//     paragraphText.textContent = `You selected ${rating} out of 5`;
+//   }
+// });
