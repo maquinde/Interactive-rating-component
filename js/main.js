e85498a713fc485d2a94a8ea5e0565ce798e0b65
Mike <maquinde@users.noreply.github.com>
2022-04-05 07:02:51 -0700

    Figured out the issue: used a class instead of ID to select the span

--- js/main.js
+++ js/main.js
@@ -1,12 +1,14 @@
-//This is for the rating selection
-//assigns all rating classes to a constant variable
+//Variables
 const choices = document.querySelectorAll('.one, .two, .three, .four, .five');
 const submit = document.querySelector('.submit-button');
 const mainCard = document.querySelector('.main-card');
 const thankYou = document.querySelector('.thank-you');
-const span = document.getElementById('#rating-selection');
+const span = document.querySelector('.rating-selection');
 let rating;
 
+
+//This is for the rating selection
+//assigns all rating classes to a constant variable
 
 //loops through all the items in variable
 for (let choice of choices){
@@ -22,39 +24,21 @@ for (let choice of choices){
         console.log('active class added');
         rating = Number(this.textContent);
         console.log(rating);
-        return rating;
     });
 }
+
+
+
 
 //For the submit button
 submit.addEventListener('click', function(){
     if(rating){
-        span.textContent = 'TEST'; //fails here
+        span.textContent = rating;
+        mainCard.classList.add('display');
+        thankYou.classList.remove('display');
     }
 
     else {
         alert('Please make a selection');
-
     }
 });
-
-
-
-//Not my code
-// 'use strict';
-
-// const ratingCard = document.querySelector('.rating-card');
-// const thankYouCard = document.querySelector('.thank-you-card');
-// const submitButton = document.querySelector('.submit');
-// const paragraphText = document.querySelector('.printed-result');
-// let rating = "";
-
-// submitButton.addEventListener('click', function () {
-//   if (!rating) {
-//     alert('Please select a rating before proceeding');
-//   } else {
-//     ratingCard.classList.add('hidden');
-//     thankYouCard.classList.remove('hidden');
-//     paragraphText.textContent = `You selected ${rating} out of 5`;
-//   }
-// });
